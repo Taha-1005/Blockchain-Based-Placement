f432cfef680278f844b6835afcd1a8f5067a24d7
Taha_Firoz <tahafbharucha@gmail.com>
2022-11-10 16:19:23 +0530

    Company Cards

--- src/frontend/components/Login/Student/ApplyInCompany.js
+++ src/frontend/components/Login/Student/ApplyInCompany.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import CompanyCard from './CompanyCard';
 
 const ApplyInCompany = () => {
   const [company, setCompany] = useState({
@@ -15,6 +16,60 @@ const ApplyInCompany = () => {
     minPpi: '',
     seatsAvailable: '',
   });
-  return <div>ApplyInCompany</div>;
+
+  const companies = [
+    {
+      eligibleBranches: ['CSE', 'EE'],
+      name: 'A',
+      ctc: 8.8,
+      onlyInternship: '0',
+      onlyJob: '0',
+      internshipAndJob: '1',
+      linkToWebsite: 'a.com',
+      location: ['PAN INDIA'],
+      category: 'A',
+      minBacklogs: '0',
+      minPpi: '7.5',
+      seatsAvailable: '',
+      post: 'Software Engineer',
+    },
+    {
+      eligibleBranches: ['ME', 'EE'],
+      name: 'B',
+      ctc: 8.5,
+      onlyInternship: '0',
+      onlyJob: '1',
+      internshipAndJob: '0',
+      linkToWebsite: 'b.com',
+      location: ['Surat', 'Banglore'],
+      category: 'A',
+      minBacklogs: '0',
+      minPpi: '7.8',
+      seatsAvailable: '10',
+      post: 'Software Engineer',
+    },
+    {
+      eligibleBranches: ['CSE', 'CH'],
+      name: 'C',
+      ctc: 11,
+      onlyInternship: '1',
+      onlyJob: '0',
+      internshipAndJob: '0',
+      linkToWebsite: 'c.com',
+      location: ['Rajkot'],
+      category: 'A',
+      minBacklogs: '0',
+      minPpi: '7.5',
+      seatsAvailable: '',
+      post: 'Software Engineer',
+    },
+  ];
+  return (
+    <div className='applyCompany'>
+      {companies.map((comp) => (
+        <CompanyCard companyData={comp} />
+      ))}
+    </div>
+  );
 };
 export default ApplyInCompany;

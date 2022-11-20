5a160020a8c7e2f5cbcb6f81ff59805579152fcd
Taha_Firoz <tahafbharucha@gmail.com>
2022-11-20 12:17:05 +0530

    Completed Company Cards

--- src/frontend/components/Login/Student/CompanyDetails.js
+++ src/frontend/components/Login/Student/CompanyDetails.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import './CompanyDetails.css'
 import { useLocation, useSearchParams } from 'react-router-dom';
 
 const CompanyDetails = () => {
   const location = useLocation();
-  const data = location.state?.data;
-  console.log(data.post);
+  const companyData = location.state?.data;
+  // console.log(companyData);
   return (
-    <div>
-      <div>details here </div>
+    <div className='details'>
+      <h1 className='title'>{companyData.name}</h1>
+      <div className='header'>Post:</div>
+      <div className='post'>{companyData.post}</div>
+      <div className='jobType'>
+        <p>Jobt Type</p>
+        <p>{companyData.post}</p>
+      </div>
+      <div className='ctc'>
+        <p>Cost to Company</p>
+        <p>{'INR ' + companyData.ctc + ' LPA'}</p>
+      </div>
+      <div className='location'>{companyData.location.join(', ')}</div>
     </div>
   );
 };
